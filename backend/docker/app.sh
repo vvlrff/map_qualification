09993cd66fccee76c37917a90a9a7e8018417dff
olegmarkelov <vvlrff@mail.ru>
2023-08-03 12:59:20 +0500

    docker compose works

--- backend/docker/app.sh
+++ backend/docker/app.sh
@@ -2,6 +2,4 @@
 
 alembic upgrade head
 
-cd app
-
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
