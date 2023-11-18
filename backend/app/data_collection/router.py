12ec7a5958ad9d8b5bb45f6730568fb82624c16f
wlrff <vvlrff@gmail.com>
2023-11-18 21:04:29 +0300

    new_backend

--- backend/app/data_collection/router.py
+++ backend/app/data_collection/router.py
@@ -6,57 +6,78 @@ from app.elastic import elastic_client
 from app.database import get_async_session
 from app.data_collection.models import news
 from app.data_collection.services import dangerous_news_guardian
-
+import logging
 
 router = APIRouter(
     prefix="/collection",
     tags=["Collection"]
 )
 
+# Настройка логгирования
+logging.basicConfig(filename='collect_news.log', level=logging.DEBUG)
+
+# Настройка логгера
+logger = logging.getLogger(__name__)
+
 
 @router.post("/collect_news")
 async def collect_news_guardian(session: AsyncSession = Depends(get_async_session)):
 
-    data = dangerous_news_guardian()
+    try:
+        data = dangerous_news_guardian()
 
-    for i in data:
-        news_title_en = i["title_en"]
-        news_title_ru = i["title_ru"]
-        news_href = i["href"]
-        news_country = i["country"]
-        news_city = i["city"]
-        news_image = i["image"]
-        news_topical_keywords = i["topical_keywords"]
-        news_date = datetime.today()
+        for i in data:
+            news_title_en = i["title_en"]
+            news_title_ru = i["title_ru"]
+            news_href = i["href"]
+            news_country = i["country"]
+            news_city = i["city"]
+            news_image = i["image"]
+            news_topical_keywords = i["topical_keywords"]
+            news_date = datetime.today()
 
-        query = select(news).where(news.c.title_en == news_title_en)
-        existing_entry = await session.execute(query)
-        existing_entry = existing_entry.fetchone()
+            # Логирование перед запросом к базе данных
+            logger.info(f"Получены данные: {i}")
 
-        if existing_entry is None:
-            stmt = insert(news).values(
-                title_en=news_title_en,
-                title_ru=news_title_ru,
-                href=news_href,
-                date=news_date,
-                city=news_city,
-                country=news_country,
-                image=news_image,
-                topical_keywords=news_topical_keywords
-            )
-            await session.execute(stmt)
-            await session.commit()
+            query = select(news).where(news.c.title_en == news_title_en)
+            existing_entry = await session.execute(query)
+            existing_entry = existing_entry.fetchone()
 
-            document = {
-                'title_en': news_title_en,
-                'title_ru': news_title_ru,
-                'href': news_href,
-                'country': news_country,
-                'city': news_city,
-                'image': news_image,
-                'topical_keywords': news_topical_keywords,
-                'date': datetime.today().isoformat()
-            }
-            await elastic_client.index(index='news', document=document)
+            if existing_entry is None:
+                stmt = insert(news).values(
+                    title_en=news_title_en,
+                    title_ru=news_title_ru,
+                    href=news_href,
+                    date=news_date,
+                    city=news_city,
+                    country=news_country,
+                    image=news_image,
+                    topical_keywords=news_topical_keywords
+                )
+                await session.execute(stmt)
+                await session.commit()
 
-    return {"status": status.HTTP_200_OK, "data": data}
+                document = {
+                    'title_en': news_title_en,
+                    'title_ru': news_title_ru,
+                    'href': news_href,
+                    'country': news_country,
+                    'city': news_city,
+                    'image': news_image,
+                    'topical_keywords': news_topical_keywords,
+                    'date': datetime.today().isoformat()
+                }
+                await elastic_client.index(index='news', document=document)
+
+                # Логирование после успешного добавления записи
+                logger.info(f"Запись добавлена в базу данных: {i}")
+            else:
+                # Логирование, если запись уже существует
+                logger.warning(f"Запись уже существует в базе данных: {i}")
+
+        return {"status": status.HTTP_200_OK, "data": data}
+
+    except Exception as e:
+        # Логирование ошибок
+        logger.error(f"Произошла ошибка: {e}")
+        return {"status": status.HTTP_500_INTERNAL_SERVER_ERROR, "error_message": str(e)}
--- backend/app/data_collection/services.py
+++ backend/app/data_collection/services.py
@@ -1,13 +1,16 @@
-import requests
-from app.data_collection.scrapper import get_news_theguardian
-from transformers import MarianMTModel, MarianTokenizer
-from PIL import Image
-import pandas as pd
-import spacy
-import numpy as np
-import pickle
 import os
+import requests
+import pandas as pd
+import numpy as np
+import spacy
+import pickle
+from transformers import MarianMTModel, MarianTokenizer
 import geonamescache
+import logging
+from app.data_collection.scrapper import get_guardian_news_items
+
+# Настройка логгирования
+logging.basicConfig(filename='news_processing.log', level=logging.DEBUG)
 
 nlp = spacy.load('en_core_web_lg')
 gc = geonamescache.GeonamesCache()
@@ -19,108 +22,87 @@ model_name_translator = "Helsinki-NLP/opus-mt-en-ru"
 model_translator = MarianMTModel.from_pretrained(model_name_translator)
 tokenizer_translator = MarianTokenizer.from_pretrained(model_name_translator)
 
+# Настройка логгера
+logger = logging.getLogger(__name__)
+
+
+def preprocess_text(text):
+    doc = nlp(text)
+    return ' '.join(token.lemma_ for token in doc if not token.is_stop and not token.is_space and not token.is_punct)
+
 
 def dangerous_news_guardian():
-    df = pd.DataFrame(get_news_theguardian())
-    preprocessing_text = []
-
-    for text in df['title']:
-        doc = nlp(text)
-        new_text = [token.lemma_ for token in doc if not token.is_stop and not token.is_space and not token.is_punct]
-        preprocessing_text.append(' '.join(new_text))
-
-    df['preprocessing_text'] = preprocessing_text
-    df['vector'] = df['preprocessing_text'].apply(lambda text: nlp(text).vector)
-    preprocessing_text = np.stack(df.vector)
+    df = pd.DataFrame(get_guardian_news_items())
+    df['preprocessing_text'] = df['title'].apply(preprocess_text)
+    df['vector'] = df['preprocessing_text'].apply(
+        lambda text: nlp(text).vector)
+    preprocessing_text = np.stack(df['vector'])
 
     with open(model_pickle_path, 'rb') as file:
         rfc = pickle.load(file)
 
     y_pred = rfc.predict(preprocessing_text)
 
-    need_list = df[y_pred == 1]['title'].tolist()
-    image_list = df[y_pred == 1]['image'].tolist()
-    url_list = df[y_pred == 1]['href'].tolist()
+    relevant_items = df[y_pred == 1]
+    need_list = relevant_items['title'].tolist()
+    image_list = relevant_items['image'].tolist()
+    url_list = relevant_items['href'].tolist()
 
-    items = []
-
-    for title, image, href in zip(need_list, image_list, url_list):
-        items.append({
-            "title": title,
-            "id": len(items),
-            "href": href,
-            "image": image
-        })
-
-    countries = gc.get_countries_by_names()
-    cities = gc.get_cities()
-
-    def extract_names(var, key):
-        if isinstance(var, dict):
-            if key in var:
-                yield var[key]
-            for k, v in var.items():
-                if isinstance(v, (dict, list)):
-                    yield from extract_names(v, key)
-        elif isinstance(var, list):
-            for d in var:
-                yield from extract_names(d, key)
-
-    countries = list(extract_names(countries, 'name'))
-    cities = list(extract_names(cities, 'name'))
-
-    items = []
+    all_news_data = []
 
     for title, image, href in zip(need_list, image_list, url_list):
         text_to_translate = title
-        inputs = tokenizer_translator.encode(text_to_translate, return_tensors="pt")
+        inputs = tokenizer_translator.encode(
+            text_to_translate, return_tensors="pt")
         translated = model_translator.generate(inputs)
-        translation = tokenizer_translator.decode(translated[0], skip_special_tokens=True)
+        translation = tokenizer_translator.decode(
+            translated[0], skip_special_tokens=True)
 
-        file_name = os.path.basename(image.split("?")[0]) 
-        save_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'photos', file_name))
+        file_name = os.path.basename(image.split("?")[0])
+        save_path = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), '..', 'photos', file_name))
 
         response = requests.get(image)
 
         if response.status_code == 200:
             with open(save_path, "wb") as file:
                 file.write(response.content)
-            print(f"Изображение {file_name} успешно загружено")
+            logger.info(f"Изображение {file_name} успешно загружено")
         else:
-            print(f"Ошибка {file_name}")
+            logger.error(f"Ошибка при загрузке изображения {file_name}")
 
         new_string = save_path.split("\\")
 
-        item = {
+        news_data = {
             "title_en": title,
             "title_ru": translation,
-            "id": len(items),
+            "id": len(all_news_data),
             "href": href,
             "image": new_string[-1],
         }
 
-        doc = nlp(title)
         found_countries = []
         found_cities = []
-        for ent in doc.ents:
+
+        for ent in nlp(title).ents:
             if ent.label_ == 'GPE':
                 country_city_name = ent.text
-                if country_city_name in countries:
+                if country_city_name in gc.get_countries_by_names():
                     found_countries.append(country_city_name)
-                elif country_city_name not in cities:
+                elif country_city_name not in gc.get_cities():
                     if country_city_name == "US":
                         found_countries.append("United States of America")
                     elif country_city_name == "UK":
                         found_countries.append("United Kingdom")
                     else:
                         found_countries.append(country_city_name)
-                if country_city_name in cities:
+                if country_city_name in gc.get_cities():
                     found_cities.append(country_city_name)
 
-        item["country"] = found_countries
-        item["city"] = found_cities
-        item["topical_keywords"] = None
-        items.append(item)
-        print(f"item{id}: {item}")
-    
-    return items
+        news_data["country"] = found_countries
+        news_data["city"] = found_cities
+        news_data["topical_keywords"] = None
+        all_news_data.append(news_data)
+        logger.info(f"Новость {news_data['id']}: {news_data} успешно создана")
+
+    return all_news_data
