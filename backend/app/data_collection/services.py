f09e521e0eacca3ad836333104956b0122b69014
wlrff <vvlrff@gmail.com>
2023-11-19 22:03:34 +0300

    победа

--- backend/app/data_collection/services.py
+++ backend/app/data_collection/services.py
@@ -1,49 +1,50 @@
 import os
 import requests
 import pandas as pd
-import numpy as np
 import spacy
-import pickle
-from transformers import MarianMTModel, MarianTokenizer
+import torch
+from transformers import MarianMTModel, MarianTokenizer, AlbertTokenizer, AlbertForSequenceClassification
 import geonamescache
 import logging
 from app.data_collection.scrapper import get_guardian_news_items
 
-# Настройка логгирования
-logging.basicConfig(filename='news_processing.log', level=logging.DEBUG)
 
-nlp = spacy.load('en_core_web_lg')
+nlp = spacy.load('en_core_web_md')
 gc = geonamescache.GeonamesCache()
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-model_pickle_path = os.path.join(current_directory, 'model_pickle')
+model_path = os.path.join(current_directory, 'albert_model')
+
+loaded_model = AlbertForSequenceClassification.from_pretrained(model_path)
+tokenizer = AlbertTokenizer.from_pretrained(model_path)
 
 model_name_translator = "Helsinki-NLP/opus-mt-en-ru"
 model_translator = MarianMTModel.from_pretrained(model_name_translator)
 tokenizer_translator = MarianTokenizer.from_pretrained(model_name_translator)
 
-# Настройка логгера
-logger = logging.getLogger(__name__)
-
-
-def preprocess_text(text):
-    doc = nlp(text)
-    return ' '.join(token.lemma_ for token in doc if not token.is_stop and not token.is_space and not token.is_punct)
-
 
 def dangerous_news_guardian():
     df = pd.DataFrame(get_guardian_news_items())
-    df['preprocessing_text'] = df['title'].apply(preprocess_text)
-    df['vector'] = df['preprocessing_text'].apply(
-        lambda text: nlp(text).vector)
-    preprocessing_text = np.stack(df['vector'])
 
-    with open(model_pickle_path, 'rb') as file:
-        rfc = pickle.load(file)
 
-    y_pred = rfc.predict(preprocessing_text)
 
-    relevant_items = df[y_pred == 1]
+    df['predicted_class'] = None
+
+    # Пройдите по каждой строке в столбце "title"
+    for index, row in df.iterrows():
+        text_to_classify = [row['title']]  # Создание списка из строки заголовка
+
+        # Токенизация и предсказание класса
+        inputs = tokenizer(text_to_classify, return_tensors="pt")
+        outputs = loaded_model(**inputs)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits, dim=1).item()
+
+        # Сохранение предсказанного класса в новую колонку
+        df.at[index, 'predicted_class'] = predicted_class
+
+
+    relevant_items = df['predicted_class' == 1]
     need_list = relevant_items['title'].tolist()
     image_list = relevant_items['image'].tolist()
     url_list = relevant_items['href'].tolist()
@@ -67,9 +68,8 @@ def dangerous_news_guardian():
         if response.status_code == 200:
             with open(save_path, "wb") as file:
                 file.write(response.content)
-            logger.info(f"Изображение {file_name} успешно загружено")
         else:
-            logger.error(f"Ошибка при загрузке изображения {file_name}")
+            logging.error(f"Ошибка при загрузке изображения {file_name}")
 
         new_string = save_path.split("\\")
 
@@ -87,22 +87,22 @@ def dangerous_news_guardian():
         for ent in nlp(title).ents:
             if ent.label_ == 'GPE':
                 country_city_name = ent.text
-                if country_city_name in gc.get_countries_by_names():
-                    found_countries.append(country_city_name)
-                elif country_city_name not in gc.get_cities():
+                if country_city_name in gc.get_cities():
+                    found_cities.append(country_city_name)
+                elif country_city_name not in gc.get_cities() and country_city_name not in gc.get_countries_by_names():
                     if country_city_name == "US":
                         found_countries.append("United States of America")
                     elif country_city_name == "UK":
                         found_countries.append("United Kingdom")
                     else:
+                        found_cities.append(country_city_name)
+                if country_city_name in gc.get_countries_by_names():
                         found_countries.append(country_city_name)
-                if country_city_name in gc.get_cities():
-                    found_cities.append(country_city_name)
 
         news_data["country"] = found_countries
         news_data["city"] = found_cities
         news_data["topical_keywords"] = None
         all_news_data.append(news_data)
-        logger.info(f"Новость {news_data['id']}: {news_data} успешно создана")
+        logging.info(f"Новость {news_data['id']}: {news_data} успешно создана")
 
     return all_news_data
